66e28921b80499116a8d600fd1edfb09bce1ff32
Christopher Amanor <kriskit7@gmail.com>
2020-11-01 14:22:34 +0000

    Add service for accessing data from API

---
+++ src/services/user.service.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'https://vehicle-booking-api.herokuapp.com/v1/';
+
+// const getPublicContent = () => {
+//   return axios.get(API_URL + 'all');
+// };
+
+// const getUserBoard = () => {
+//   return axios.get(API_URL + 'user', { headers: authHeader() });
+// };
+
+// const getModeratorBoard = () => {
+//   return axios.get(API_URL + 'mod', { headers: authHeader() });
+// };
+
+// const getAdminBoard = () => {
+//   return axios.get(API_URL + 'admin', { headers: authHeader() });
+// };
+
+const getVehicles = () => axios.get(`${API_URL}vehicles`, { headers: authHeader() });
+
+export default {
+  getVehicles,
+};
