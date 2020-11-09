ba3766d0ba02d21886f9585f7128f982553f94a6
Christopher Amanor <kriskit7@gmail.com>
2020-11-09 11:04:21 +0000

    Refactor Vehicle.js

--- src/components/Vehicle.js
+++ src/components/Vehicle.js
@@ -1,14 +1,9 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/style-prop-object */
-/* eslint-disable jsx-a11y/img-redundant-alt */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const Vehicle = ({ vehicle }) => {
   const {
     name, photo,
-    // model, transmission, fee,
   } = vehicle;
 
   return (
@@ -16,25 +11,13 @@ const Vehicle = ({ vehicle }) => {
       <img
         className="card-img-top"
         src={photo}
-        alt="Card image"
+        alt=""
       />
       <div className="card-body">
         <h4 className="card-title">{name}</h4>
-        {/* <p className="card-text">
-          Model:
-          {model}
-        </p>
-        <p className="card-text">
-          Transmission:
-          {transmission}
-        </p>
-        <p className="card-text">
-          Fee:
-          {fee}
-        </p> */}
-        <a href="#" className="btn btn-primary">
+        <button type="button" className="btn btn-primary">
           See Details
-        </a>
+        </button>
       </div>
     </div>
   );
@@ -45,8 +28,6 @@ export default Vehicle;
 Vehicle.propTypes = {
   vehicle: PropTypes.shape({
     name: PropTypes.string,
-    model: PropTypes.string,
-    transmission: PropTypes.string,
-    fee: PropTypes.string,
+    photo: PropTypes.string,
   }).isRequired,
 };
